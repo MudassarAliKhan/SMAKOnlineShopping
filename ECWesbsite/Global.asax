9fca920e7dff30ee6899add54bdbc32a8496f4cb
MudassarAliKhan <mudassaraliofficial@gmail.com>
2020-08-18 16:18:33 +0500

    Add project files.

---
+++ ECWesbsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECWesbsite.MvcApplication" Language="C#" %>
---
+++ ECWesbsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECWesbsite.Startup))]
+namespace ECWesbsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
